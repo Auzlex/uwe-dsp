f4e0a9f757d85f0b72cf0a0d9e1fd6cc877154ea
Auzlex <edwardscharlesuk@googlemail.com>
2022-01-29 13:44:08 +0000

    more CNN testing

--- audio_cnn_test/helper.py
+++ audio_cnn_test/helper.py
@@ -1,4 +1,41 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+# Plots a confussion matrix
+def plot_confusion_matrix(cm,
+                          classes, 
+                          normalized=False, 
+                          title=None, 
+                          cmap=plt.cm.Blues,
+                          size=(10,10)):
+    fig, ax = plt.subplots(figsize=size)
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalized else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+
+    fig.tight_layout()
+    plt.show()
 
 # padding function
 # Given an numpy array of features, zero-pads each occurrence to max_padding
